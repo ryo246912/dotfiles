2e05e4b88c4fc2353baeb0dcfb90b4bf95f4c23f
kuramitsu-v <110804674+kuramitsu-v@users.noreply.github.com>
2023-01-28 00:02:14 +0900

    add : initial commit

---
+++ dot_hammerspoon/init.lua
@@ -0,0 +1,39 @@
+hs.hotkey.bind({"alt"}, "A", function()
+  hs.application.launchOrFocus("Alacritty")
+end)
+
+hs.hotkey.bind({"alt"}, "C", function()
+  hs.application.launchOrFocus("Google Chrome")
+end)
+
+hs.hotkey.bind({"alt"}, "D", function()
+  hs.application.launchOrFocus("DBeaver")
+end)
+
+hs.hotkey.bind({"alt"}, "F", function()
+  hs.application.launchOrFocus("Finder")
+end)
+
+hs.hotkey.bind({"alt"}, "G", function()
+  hs.application.launchOrFocus("Gather")
+end)
+
+hs.hotkey.bind({"alt"}, "K", function()
+  hs.application.launchOrFocus("KeePassXC")
+end)
+
+hs.hotkey.bind({"alt"}, "N", function()
+  hs.application.launchOrFocus("Notion")
+end)
+
+hs.hotkey.bind({"alt"}, "S", function()
+  hs.application.launchOrFocus("Slack")
+end)
+
+hs.hotkey.bind({"alt"}, "V", function()
+  hs.application.launchOrFocus("Visual Studio Code")
+end)
+
+hs.hotkey.bind({"alt"}, "Z", function()
+  hs.application.launchOrFocus("System Preferences")
+end)
