52b5beac84e7a1936ee769e73daf4fd995278455
ryo246912 <r-k.sky_25cloudf@hotmail.co.jp>
2025-05-29 09:43:17 +0900

    chore: powershellのaliasを追加

--- dot_config/zsh/lazy/wsl.zsh
+++ dot_config/zsh/lazy/wsl.zsh
@@ -9,3 +9,7 @@ gpgconf --launch gpg-agent
 # fly
 export FLYCTL_INSTALL="$HOME/.fly"
 export PATH="$FLYCTL_INSTALL/bin:$PATH"
+
+# powershell
+alias pwsh="/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
+alias powershell="/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
