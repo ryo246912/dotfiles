2026d5680a1e73c510e9004cdca4405ea5da4e5a
ryo246912 <r-k.sky_25cloudf@hotmail.co.jp>
2025-10-12 23:12:04 +0900

    chore(vim): add hardtime,nvim-notify

--- dot_config/nvim/lua/plugins/util.lua
+++ dot_config/nvim/lua/plugins/util.lua
@@ -7,6 +7,23 @@ return {
       opts = {},
   },
   {
+    "rcarriga/nvim-notify",
+    config = function()
+      require("notify").setup({
+        background_colour = "#000000",
+      })
+      vim.notify = require("notify")
+    end,
+  },
+  {
+    "m4xshen/hardtime.nvim",
+    lazy = false,
+    dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
+    opts = {
+      max_count = 10,
+    },
+  },
+  {
     "vim-jp/vimdoc-ja",
     lazy = true,
   },
