97275421b204b55473dd9e3abe7e962abe4dc4ca
ryo246912 <r-k.sky_25cloudf@hotmail.co.jp>
2026-03-09 01:34:27 +0900

    fix: review

--- dot_config/nvim/lua/plugins/github.lua
+++ dot_config/nvim/lua/plugins/github.lua
@@ -7,6 +7,13 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
     cmd = "Octo",
+    keys = {
+      { "<leader>op", "<Cmd>Octo pr list<CR>",      mode = "n", silent = true, desc = "PR一覧" },
+      { "<leader>oc", "<Cmd>Octo pr create<CR>",    mode = "n", silent = true, desc = "PR作成" },
+      { "<leader>or", "<Cmd>Octo review start<CR>", mode = "n", silent = true, desc = "PRレビュー開始" },
+      { "<leader>oi", "<Cmd>Octo issue list<CR>",   mode = "n", silent = true, desc = "Issue一覧" },
+      { "<leader>oI", "<Cmd>Octo issue create<CR>", mode = "n", silent = true, desc = "Issue作成" },
+    },
     config = function()
       require("octo").setup({
         picker = "fzf-lua",
@@ -14,17 +21,6 @@ return {
         -- GitHub Enterprise の場合は以下を設定
         -- github_hostname = "github.example.com",
       })
-
-      local keymap = vim.keymap.set
-
-      -- PR 操作
-      keymap("n", "<leader>op", "<Cmd>Octo pr list<CR>",       { noremap = true, silent = true, desc = "PR一覧" })
-      keymap("n", "<leader>oc", "<Cmd>Octo pr create<CR>",     { noremap = true, silent = true, desc = "PR作成" })
-      keymap("n", "<leader>or", "<Cmd>Octo review start<CR>",  { noremap = true, silent = true, desc = "PRレビュー開始" })
-
-      -- Issue 操作
-      keymap("n", "<leader>oi", "<Cmd>Octo issue list<CR>",    { noremap = true, silent = true, desc = "Issue一覧" })
-      keymap("n", "<leader>oI", "<Cmd>Octo issue create<CR>",  { noremap = true, silent = true, desc = "Issue作成" })
     end,
   },
 }
