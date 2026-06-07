cb33312dd57d97f95af944a31808e84f992b3c57
ryo246912 <r-k.sky_25cloudf@hotmail.co.jp>
2026-06-08 01:37:29 +0900

    chore: add which key

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -1,5 +1,14 @@
 return {
   {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("which-key").setup({
+        delay = 500,
+      })
+    end,
+  },
+  {
     "windwp/nvim-autopairs",
     config = function()
       require("nvim-autopairs").setup({})
