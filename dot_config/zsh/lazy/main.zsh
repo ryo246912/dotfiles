8a004e6045906169c792405bb3c81a8d907142a9
ryo. <73044070+ryo246912@users.noreply.github.com>
2026-09-15 21:50:36 +0900

    Update main.zsh

--- dot_config/zsh/lazy/main.zsh
+++ dot_config/zsh/lazy/main.zsh
@@ -9,7 +9,6 @@ mkdir -p "$XDG_STATE_HOME/psql" 2>/dev/null
 export PSQL_HISTORY="$XDG_STATE_HOME/psql/.psql_history"
 mkdir -p "$XDG_STATE_HOME/less" 2>/dev/null
 export LESSHISTFILE="$XDG_STATE_HOME/less/.lesshst"
-# NOTE: zsh の HISTFILE は遅延読込だと起動時の履歴読込に間に合わないため main.zsh で設定する
 
 # slack
 export SLACK_DEVELOPER_MENU=true
